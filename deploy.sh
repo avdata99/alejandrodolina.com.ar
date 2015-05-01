76683cd4a22945daf3e7c94868ab69e9d4c1eb97
avdata99 <andres@data99.com.ar>
2015-04-30 21:41:21 -0300

    fix and add deploy script

---
+++ deploy.sh
@@ -0,0 +1,8 @@
+#files
+echo "Rsync files"
+rsync -av --progress --exclude='.git' --rsh='ssh -p 987' \
+	. dolina@alejandrodolina.com.ar:/home/dolina/www/
+ 
+#permissions
+echo "Fix file's permissions"
+ssh -p 987 dolina@alejandrodolina.com.ar "find /home/dolina/www/ -type d -exec chmod 755 {} \; && find /home/dolina/www/ -type f -exec chmod 644 {} \;"
